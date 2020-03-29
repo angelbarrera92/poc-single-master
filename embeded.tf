af7903b3d2df333ce2a2449cf135a490ee3703e8
Angel Barrera Sanchez <angelbarrerasanchez@protonmail.com>
2020-03-29 19:42:23 +0200

    Add two different kubernetes clusters

---
+++ embeded.tf
@@ -0,0 +1,31 @@
+module "embeded" {
+  source = "./modules/aws-k8s-kubeadm"
+
+  region = data.aws_region.current.name
+
+  cluster_name    = "embeded"
+  cluster_version = "1.16.8"
+
+  worker_count = 2
+
+  public_subnet_id  = "subnet-3253677a"
+  private_subnet_id = "subnet-d9c4e1bf"
+
+}
+
+output "embeded_tls_private_key" {
+  sensitive   = true
+  description = "Private RSA Key to log into the control plane node"
+  value       = module.embeded.tls_private_key
+}
+
+output "embeded_master_public_ip" {
+  description = "Public IP where control plane is exposed"
+  value       = module.embeded.master_public_ip
+}
+
+output "embeded_worker_private_ip" {
+  description = "Worker nodes private ip list"
+  value       = module.embeded.worker_private_ip
+}
+
---
+++ external.tf
@@ -0,0 +1,31 @@
+module "external" {
+  source = "./modules/aws-k8s-kubeadm"
+
+  region = data.aws_region.current.name
+
+  cluster_name    = "external"
+  cluster_version = "1.16.8"
+
+  worker_count = 2
+  external_db  = true
+
+  public_subnet_id  = "subnet-3253677a"
+  private_subnet_id = "subnet-d9c4e1bf"
+
+}
+
+output "external_tls_private_key" {
+  sensitive   = true
+  description = "Private RSA Key to log into the control plane node"
+  value       = module.external.tls_private_key
+}
+
+output "external_master_public_ip" {
+  description = "Public IP where control plane is exposed"
+  value       = module.external.master_public_ip
+}
+
+output "external_worker_private_ip" {
+  description = "Worker nodes private ip list"
+  value       = module.external.worker_private_ip
+}
